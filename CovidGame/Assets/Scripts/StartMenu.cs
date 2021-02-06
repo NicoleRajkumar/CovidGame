ec4c663d1ae9fef65cb0b3d5bdd6559020876564
NicoleRajkumar <nicolerajkumar@hotmail.com>
2021-02-06 00:08:37 -0500

    added Start Menu and Main Game

---
+++ CovidGame/Assets/Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class StartMenu : MonoBehaviour
+{
+    public Button enterButton;
+    public bool isClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("nani");
+        isClicked = false;
+        Button btn = enterButton.GetComponent<Button>();
+        btn.onClick.AddListener(StartGame);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void StartGame()
+    {
+        Debug.Log("You have clicked the button!");
+        isClicked = true;
+        SceneManager.LoadScene("MainGame");
+    }
+}
